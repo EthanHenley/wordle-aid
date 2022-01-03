799336962a6f9eaaa283bbb3c4d628761aa186e1
EthanHenley <ethanhenley@gmail.com>
2022-01-03 17:44:52 -0500

    Fixed backwards Levenshtein bug, implemented automatic/fun modes.

--- wordle.py
+++ wordle.py
@@ -1,29 +1,53 @@
 from nltk.corpus import words
 from nltk import edit_distance
+from random import choice
 
 corpus = [w.lower() for w in words.words() if len(w)==5]
 alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
 required = []
 win = False
+live = False
+convenience = {2:'1',1:'0',0:'N'}
+
+# set mode
+wtype = ''
+while wtype == '':
+	print('Word options: INPUT | RANDOM | LIVE')
+	wtype = input('Word type?: ').upper()
+	if wtype == 'INPUT':
+		secret = input('Input secret word: ').lower()
+		secret = secret[:5]
+	elif wtype == 'RANDOM':
+		secret = choice(corpus)
+	elif wtype == 'LIVE':
+		live = True
+	else:
+		wtype = ''
 
 for i in range(6):
 	# read guess, outcome
 	if i==0:
-		instr = 'Guess? F{EARTH}:'
+		instr = 'Guess? F{EARTH}: '
 	else:
-		instr = 'Guess?:'
+		instr = 'Guess?: '
 	guess = input(instr).lower()
 
-	if i==0:
-		instr = 'Outcome? F{N100N}:'
-	else:
-		instr = 'Outcome?:'
-	outcome = input(instr).lower()
-
+	if live: # only input outcome codes if playing live game
+		if i==0:
+			instr = 'Outcome? F{N100N}: '
+		else:
+			instr = 'Outcome?: '
+		outcome = input(instr).lower()
+	else: # simulated, slightly broken version
+		outcome = ''
+		for (cga, sga) in zip(guess, secret):
+			outcome += convenience[int((cga==sga)+(cga in secret))] # slight error with how this handles double letters
+		print('Outcome:',outcome)
+		outcome = outcome.lower()
 	if outcome=='11111':
-		print('Congratulations!')
-		win = True
-		break
+			print('Congratulations! Score:',i+1)
+			win = True
+			break
 		
 	# trim possibilities
 	for ig, (cg, og) in enumerate(zip(guess, outcome)):
@@ -42,8 +66,8 @@ for i in range(6):
 	print('Required:',''.join(set(required)).upper())
 	optimalstring = ''.join([max(alphabet, key=[w[i] for w in corpus].count) for i in range(5)]).upper()
 	print('Optimal placement:',optimalstring)
-	optimallevenshtein = lambda w: edit_distance(w, optimalstring)#, True)
-	print('Recommended word:',max(corpus, key=optimallevenshtein).upper())
+	optimallevenshtein = lambda w: edit_distance(w.lower(), optimalstring.lower())#, True)
+	print('Recommended word:',min(corpus, key=optimallevenshtein).upper())
 
 	if len(corpus) < 50:
 		print('Remaining wordspace:')
